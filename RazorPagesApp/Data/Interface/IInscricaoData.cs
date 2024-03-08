ac1e282dc1ad679168c0acbb99a6c09933846b41
Gilson Fonseca <gf.gilsonfonseca@gmail.com>
2024-03-08 19:07:31 -0300

    feat: criação da classe IInscricaoData

---
+++ RazorPagesApp/Data/Interface/IInscricaoData.cs
@@ -0,0 +1,17 @@
+﻿using RazorPagesApp.Models;
+
+namespace RazorPagesApp.Data.Interface
+{
+    public interface IInscricaoData
+    {
+        public Task<int> Inserir(InscricaoModel inscricao);
+
+        public Task Alterar(InscricaoModel inscricao);
+
+        public Task Inativar(int id);
+
+        public Task<InscricaoModel> ConsultarPorId(int id);
+
+        public Task<IEnumerable<InscricaoModel>> Listar();
+    }
+}
