c4c11870626d8383cac19b8d78d9b278debed090
Gilson Fonseca <gf.gilsonfonseca@gmail.com>
2024-03-08 19:09:56 -0300

    feat: criação da classe InscricaoDataApi

---
+++ RazorPagesApp/Data/InscricaoDataApi.cs
@@ -0,0 +1,34 @@
+﻿using RazorPagesApp.HttpClient.Interface;
+using RazorPagesApp.Models;
+
+namespace RazorPagesApp.Data
+{
+    public class InscricaoDataApi : Interface.IInscricaoData
+    {
+
+        public Task Alterar(InscricaoModel inscricao)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<InscricaoModel> ConsultarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Inativar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Inserir(InscricaoModel inscricao)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<InscricaoModel>> Listar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
