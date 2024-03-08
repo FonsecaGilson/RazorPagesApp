e2a29bc09dbc040803a7ed4387f9e81adaffdbea
Gilson Fonseca <gf.gilsonfonseca@gmail.com>
2024-03-08 19:12:08 -0300

    feat: criação da interface IInscricaoHttpClient

---
+++ RazorPagesApp/HttpClient/Interface/IInscricaoHttpClient.cs
@@ -0,0 +1,23 @@
+﻿using RazorPagesApp.Models;
+using Refit;
+
+namespace RazorPagesApp.HttpClient.Interface
+{
+    public interface IInscricaoHttpClient
+    {
+        [Post("/Inserir")]
+        Task<int> Inserir(InscricaoModel inscricao);
+
+        [Put("/Alterar")]
+        Task Alterar(InscricaoModel inscricao);
+
+        [Delete("/Inativar")]
+        Task Inativar(int id);
+
+        [Get("/ConsultarPorId")]
+        Task<InscricaoModel> ConsultarPorId(int id);
+
+        [Get("/Listar")]
+        Task<IEnumerable<InscricaoModel>> Listar();
+    }
+}
